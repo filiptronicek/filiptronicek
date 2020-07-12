77a5252aced22e84f2241c9564c07c81fbc540aa
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-12 22:20:08 +0200

    Add template to README

--- blog.py
+++ blog.py
@@ -2,6 +2,9 @@ import requests
 from os import getenv
 import datetime
 from dotenv import load_dotenv
+
+from modify import write
+
 load_dotenv()
 
 user = "filiptronicek"
@@ -12,9 +15,22 @@ headers = {'Authorization': getenv("TOKEN")}
 r = requests.get(url, headers=headers)
 res = r.json()
 
+posts = []
+
 for file in res["tree"]:
     if "_posts/" in file["path"]:
-        fileNm = file["path"].split("/")[1]
+        fileNm = file["path"].split("/")[-1]
         dateStr = fileNm[:10]
         date_time_obj = datetime.datetime.strptime(dateStr, "%Y-%M-%d")
-        print(date_time_obj.strftime("%B %d, %Y"))
+        fmtTime = date_time_obj.strftime("%B %d, %Y")
+        flUrl = f"https://raw.githubusercontent.com/filiptronicek/filiptronicek.github.io/master/_posts/{fileNm}"
+        flReq = requests.get(flUrl).text
+        if "title" in flReq.strip().split("\n")[1]:
+            title = flReq.strip().split("\n")[1]
+        elif "title" in flReq.strip().split("\n")[2]:
+            title = flReq.strip().split("\n")[2]
+        else:
+            title = fileNm
+        blogUrl = f"https://blog.trnck.dev/{fileNm[11:].replace('.md','/')}"
+        posts.append({"time": fmtTime, "title": title, "url": blogUrl})
+write(posts[-1]["title"],posts[-1]["url"], posts[-1]["time"])
--- modify.py
+++ modify.py
@@ -1,9 +1,9 @@
 import re
 
 filename = "README.md"
-patt = r'<a.*class="blog".*>.*</a>'
+patt = r'<a.*class="post".*>.*</a>'
 
-def write(rank):
+def write(rank, url, date):
     with open(filename) as f:
         content = f.readlines()
     content = [x for x in content] 
@@ -11,6 +11,7 @@ def write(rank):
     for c in content:
         if re.search(patt, c):
             splitToModify = (c.split(re.split(patt, c)[0])[1])
+            splitToModify = splitToModify.replace('href="https://blog.trnck.dev/"', f'href="{url}"')
             modify = re.sub(r">[\d]+?<",f">{str(rank)}<", splitToModify)
             replaced += c.split(splitToModify)[0] + modify + c.split(splitToModify)[1]
         else:
