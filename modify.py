805e0a010c56b9fed6d9b8b5cc00cad58968c70c
filiptronicek <filip@gitpod.io>
2023-06-21 18:00:00 +0000

    Fix `modify.py`

--- modify.py
+++ modify.py
@@ -5,21 +5,12 @@ patt = r'<a class="post" href="https:\/\/blog.trnck.dev\/.*\/">.*<\/a>'
 
 
 def write(title, url, date):
-    with open(filename, encoding="utf-8") as f:
-        content = f.readlines()
-    content = [x for x in content]
-    replaced = ""
-    for c in content:
-        if re.search(patt, c):
-            print("found")
-            splitToModify = c.split(re.split(patt, c)[0])[1]
-            splitToModify = re.sub(r'href="https://blog.trnck.dev/[.*]?"',
-                                   f'href="{url}"', splitToModify)
-            modify = re.sub(r">.*<", f">{str(title)} (published on {date})<",
-                            splitToModify)
-            replaced += (c.split(splitToModify)[0]).split("<")[0] + modify
-            replaced = re.sub(r'\"https://blog.trnck.dev/.*\"', f"\"{url}\"", replaced)
-        else:
-            replaced += c
+    with open(filename, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    replaced = re.sub(
+        patt, f'<a class="post" href="{url}">{title} (published on {date})</a>', content
+    )
+
     with open(filename, "w", encoding="utf-8") as f:
         f.write(replaced)
